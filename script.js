0dc63081823039a94e03b0854c2423176961570d
chaima marzouk <marzouk.chaima.892@gmail.com>
2021-11-26 09:14:04 +0100

    Add error alerts

--- script.js
+++ script.js
@@ -31,19 +31,12 @@ let operations ={
 
 }
 
-let sign = null;
 
-
-/**
- * Effectue le calcul
- *                                                                                                                                        
- */
 calculate = function (val1, val2, sign) {
     if (sign in operations) {
         
         return operations[sign](val1, val2);
     }
-
 }
 
 // console.log(calculate(1,2,'*'))//TEST§§§§§§§§
@@ -58,7 +51,18 @@ const showScreen = document.querySelector("#output")
 //On stock la valeur de l'ecran precedent 
     let backThen = 0;
 
+//On stock la signe ou l'operateur 
+    let sign = null;
+
+//On stock le nombre des virgules ou plutot les points qu'un chiffre contien
+    // let commaBackThen = parseString.backThen.split(".").length -1;
+    let commaOutput = output.length ;
+
 //add event listner to listen to button beig presed by the user
+
+    function ON(){
+        showScreen.innerText = "0";
+    }
 
      //listen to clicks 
      let touches = document.querySelectorAll("button");
@@ -69,20 +73,17 @@ const showScreen = document.querySelector("#output")
 
 
 
- function controleTouches(){
-    //  html button element
-     let touche = this.innerText;
+    function controleTouches(){
+        //  html button element
+        let touche = this.innerText;
 
-      //verfier si un chiffre ou un point 
-      if(parseFloat(touche)>= 0 || touche === "."){
-           
-          //je met a jour la valeur d'affichage 
-          output +=  touche.toString()   
-          showScreen.innerText = output ;
-
-        
-         
-      } else{
+        //verfier si un chiffre ou un point 
+        if(parseFloat(touche)>= 0 || touche === "."){
+            
+        //je met a jour la valeur d'affichage 
+        output +=  touche.toString()   
+        showScreen.innerText = output ;
+    }else{
           switch(touche){
               //Touche Clear reinitialise tout !!
               case "Clear":
@@ -94,29 +95,49 @@ const showScreen = document.querySelector("#output")
               case "+":
               case "-":
               case "*":   
-              case "/": 
+              case "/":
+                  
+               if(commaOutput > 3){
+                alert("Please check your operation can't contanet more then 1 comma ! please check your operation  and try again , thank you");
+                backThen = 0
+                output = ""
+                showScreen.innerText = 0
+                break
+              }else{
 
-                    backThen = (backThen === 0) ? parseFloat(output) : calculate(backThen , parseFloat(output), sign);
-                    console.log(backThen)
-                    showScreen.innerText = backThen;
-                    sign = touche;
-                    output="";
-                    break;
+                backThen = (backThen === 0) ? parseFloat(output) : calculate(backThen , parseFloat(output), sign);
+                console.log(backThen)
+                showScreen.innerText = backThen;
+                sign = touche;
+                console.log(sign)
+                output="";
+                break;
+              }
+
 
               case "=":
               case "Enter":
-                backThen = (backThen === 0) ? parseFloat(output) : calculate(backThen , parseFloat(output), sign);
-                 // je met a jour l ecran
-                 showScreen.innerText = backThen;
-                //je  stocke le resultat dans la variable d'affichage
-                output = backThen;
-
-                // On réinitialise précédent
-                backThen = 0;
-                break;
+                if (backThen === 0 || sign == null || output === "" ) {
+                    alert("Please check your operation and try again! thank you");
+                    backThen = 0
+                    output = ""
+                    showScreen.innerText = 0
+                    break
+                  }else{
+                      backThen = (backThen === 0) ? parseFloat(output) : calculate(backThen , parseFloat(output), sign);
+                       // je met a jour l ecran
+                       showScreen.innerText = backThen;
+                      //je  stocke le resultat dans la variable d'affichage
+                      output = backThen;
+                      // On réinitialise précédent
+                      backThen = 0;
+                      break;
+                }
           }
       }
 
 
  }
+
+
 
